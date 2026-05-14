b29243da165fdb5c7e43e9c5cc725a818d80e117
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-05-15 02:59:21 +0600

    3rd add

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navber from '../Component/navBer/Navber';
 import { Outlet } from 'react-router';
+import Footer from '../Component/footer/Footer';
 
 const MainLayout = () => {
       return (
             <div>
                   <Navber></Navber>
+
                   <Outlet></Outlet>
+                  <Footer></Footer>
 
             </div>
       );
