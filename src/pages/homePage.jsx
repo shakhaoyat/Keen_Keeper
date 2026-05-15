03b7a00bc5236cc00626cfca74c4c25069ac15a9
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-05-16 04:48:00 +0600

    card

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { use } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import AllFriends from '../Component/homePage/AllFriends';
 
+const friendsPromise = fetch('/friends.json').then(res => res.json());
+
+
 const homePage = () => {
+      const friends = use(friendsPromise);
+      const totalFriends = friends.length;
+      const onTrack = friends.filter(f => f.status === "on-track").length;
+      const needAttention = friends.filter(f => f.status === "overdue" || f.status === "almost due").length;
+      const interactionsThisMonth = friends.filter(f => f.days_since_contact <= 30).length;
       return (
             <div>
                   <div>
@@ -22,7 +30,7 @@ const homePage = () => {
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 py-6 container mx-auto ">
                         <div className="card w-full bg-base-100 card-xl shadow-sm">
                               <div className="card-body items-center text-center">
-                                    <h2 className="card-title items-center ">Xlarge Card</h2>
+                                    <h2 className="card-title items-center ">{totalFriends}</h2>
                                     <p>Total Frienda</p>
                                     <div className="justify-end card-actions">
 
@@ -31,8 +39,8 @@ const homePage = () => {
                         </div>
                         <div className="card w-full bg-base-100 card-xl shadow-sm">
                               <div className="card-body items-center text-center">
-                                    <h2 className="card-title items-center ">Xlarge Card</h2>
-                                    <p>Total Frienda</p>
+                                    <h2 className="card-title items-center ">{onTrack}</h2>
+                                    <p>On Track</p>
                                     <div className="justify-end card-actions">
 
                                     </div>
@@ -40,8 +48,8 @@ const homePage = () => {
                         </div>
                         <div className="card w-full bg-base-100 card-xl shadow-sm">
                               <div className="card-body items-center text-center">
-                                    <h2 className="card-title items-center ">Xlarge Card</h2>
-                                    <p>Total Frienda</p>
+                                    <h2 className="card-title items-center ">{needAttention}</h2>
+                                    <p>Need Attention</p>
                                     <div className="justify-end card-actions">
 
                                     </div>
@@ -49,8 +57,8 @@ const homePage = () => {
                         </div>
                         <div className="card w-full bg-base-100 card-xl shadow-sm">
                               <div className="card-body items-center text-center">
-                                    <h2 className="card-title items-center ">Xlarge Card</h2>
-                                    <p>Total Frienda</p>
+                                    <h2 className="card-title items-center ">{interactionsThisMonth}</h2>
+                                    <p>Interactions This Month</p>
                                     <div className="justify-end card-actions">
 
                                     </div>
